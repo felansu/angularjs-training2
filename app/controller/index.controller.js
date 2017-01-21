63fa455fa918098b15304734150238020a50bce2
Ferran Gonzalez Alonso <gaferran@gmail.com>
2017-01-21 11:58:07 -0200

    first commit

---
+++ app/controller/index.controller.js
@@ -0,0 +1,45 @@
+angular
+    .module('pdCurso')
+    .controller('IndexController', IndexController);
+
+function IndexController($scope) {
+
+    // Declaração de variáveis
+    $scope.pessoa = {};
+    $scope.listaPessoas = [];
+
+    // Publicação de funções
+    $scope.onClickBotao = onClickBotao;
+    $scope.outraFunction = outraFunction;
+
+    $scope.salvar = salvar;
+    $scope.limpar = limpar;
+    $scope.excluir = excluir;
+    $scope.editar = editar;
+
+    // Declaração de funções
+    function onClickBotao() {
+        alert("Opa, funcao 1")
+    }
+
+    function outraFunction() {
+        alert("Opa, funcao 2")
+    }
+
+    function salvar() {
+        $scope.listaPessoas.push($scope.pessoa);
+        $scope.pessoa = {};
+    }
+
+    function limpar() {
+        $scope.pessoa = {};
+    }
+
+    function excluir(index) {
+        $scope.listaPessoas.splice(index, 1);
+    }
+
+    function editar(pessoa) {
+        $scope.pessoa = pessoa;
+    }
+}
