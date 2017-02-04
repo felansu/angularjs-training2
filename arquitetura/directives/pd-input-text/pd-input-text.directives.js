f53913f47bb5d85c25ebc496d1eec5a25ff168e8
felansu <gaferran@gmail.com>
2017-02-04 17:01:18 -0200

    - directiva 3 | Directiva com validação genêrica de inputs

--- arquitetura/directives/pd-input-text/pd-input-text.directives.js
+++ arquitetura/directives/pd-input-text/pd-input-text.directives.js
@@ -9,18 +9,22 @@
     function pdInputText() {
         var directive = {
             restrict: 'E',
+            require: '^form',
             templateUrl: 'arquitetura/directives/pd-input-text/pd-input-text.html',
             link: link,
             scope: {
                 label: '@',
                 ngModel: '=',
-                ngRequired: '='
+                ngRequired: '=',
+                colspan: '@'
             }
         };
         return directive;
 
-        function link(scope, element, attrs) {
-
+        function link($scope, element, attrs, formCtrl) {
+            $scope.formCtrl = formCtrl;
+            $scope.classColspan = 'col-md-' + ($scope.colspan || '3');
+            $scope.inputName = 'pdInputText' + $scope.id;
         }
     }
 
